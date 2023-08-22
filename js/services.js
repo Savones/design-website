17a56bcac3d7ceccfd0b00b8b3961c66b72548f4
Essi Savonius <essi.savonius@hotmail.fi>
2023-08-22 03:09:41 +0300

    services updates

--- js/services.js
+++ js/services.js
@@ -3,14 +3,19 @@ let openPackage = new Set();
 function showDetails(packageNro) {
     const packageList = document.getElementById(`${packageNro}`);
     const packageItem = packageList.parentElement;
+    let buttonClicked = packageItem.querySelector(`.${packageNro}`);
 
     if (openPackage.has(packageList)) {
+        packageItem.classList.remove('expanded');
         packageList.classList.add('hidden-container');
         packageItem.style.height = '230px';
         openPackage.delete(packageList)
+        buttonClicked.innerHTML = 'Details';
     } else {
+        packageItem.classList.add('expanded');
         packageList.classList.remove('hidden-container');
         packageItem.style.height = `auto`;
         openPackage.add(packageList);
+        buttonClicked.innerHTML = 'Hide';
     }
 }
